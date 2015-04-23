3389a22fb87de8b9721c5f7b3ac858ce05abd719
XiaoWeiWei <chronicle2nd_ww@hotmail.com>
2015-04-23 10:44:19 +0800

    增加删除对应行

--- csv.go
+++ csv.go
@@ -1,8 +1,10 @@
 package csv
 
 import (
+	"bufio"
 	"encoding/csv"
 	"os"
+	"time"
 )
 
 // 读取.csv文件，舍弃空行，舍弃id为空的行(包含舍弃由",,,,"构成的空行，)。
@@ -46,5 +48,56 @@ func Write(filename string, records [][]string) (e error) {
 		return e
 	}
 
+	return nil
+}
+
+func reWrite(filename string, records [][]string) (e error) {
+	file, e := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0777)
+	if e != nil {
+		return e
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	e = writer.WriteAll(records)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func DelRecord(filename string, records [][]string) error { //注释某行
+	file, e := os.OpenFile(filename, os.O_RDWR, 0777)
+	if e != nil {
+		return e
+	}
+	defer file.Close()
+
+	recOld, e := Read(filename)
+	if e != nil {
+		return e
+	}
+	// k := 0
+	j := len(recOld) - 1
+	for i, v := range recOld {
+		for _, u := range records {
+			if u[0] == v[0] {
+				tmprec := recOld[j]
+				recOld[j] = recOld[i]
+				recOld[i] = tmprec
+				j--
+				break
+			}
+		}
+		if !(i < j) {
+			break
+		}
+	}
+	e = reWrite(filename, recOld[:j])
+	if e != nil {
+		return e
+	}
 	return nil
 }
